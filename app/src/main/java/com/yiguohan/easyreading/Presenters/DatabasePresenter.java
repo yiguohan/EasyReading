37e64e17e5a6cc3c7817aae0613e824174c016a2
yiguohan <yiguohan@gmail.com>
2017-05-17 15:08:00 +0800

    调整了我的阅读的Item布局

---
+++ app/src/main/java/com/yiguohan/easyreading/Presenters/DatabasePresenter.java
@@ -0,0 +1,17 @@
+package com.yiguohan.easyreading.Presenters;
+
+import android.content.Context;
+
+import com.yiguohan.easyreading.Base.BasePresenter;
+
+/**
+ * Created by yiguohan on 2017/5/17.
+ * Email:yiguohan@gmail.com
+ */
+
+public class DatabasePresenter extends BasePresenter {
+
+    public DatabasePresenter(Context context) {
+        super(context);
+    }
+}
