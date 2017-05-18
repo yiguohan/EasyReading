3d7f14d34ff754309731646c52eae5523971d3f3
yiguohan <yiguohan@gmail.com>
2017-05-18 19:49:40 +0800

    调整DoubanBookDetailActivity基本布局

--- app/src/main/java/com/yiguohan/easyreading/ViewImpls/DoubanBookDetailActivity.java
+++ app/src/main/java/com/yiguohan/easyreading/ViewImpls/DoubanBookDetailActivity.java
@@ -1,37 +1,56 @@
 package com.yiguohan.easyreading.ViewImpls;
 
 import android.content.Intent;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.yiguohan.easyreading.Beans.DoubanBooks.Book;
 import com.yiguohan.easyreading.Presenters.DoubanBooksPresenter;
 import com.yiguohan.easyreading.R;
 import com.yiguohan.easyreading.Views.IGetBookView;
 
-public class DoubanBookDetailActivity extends AppCompatActivity implements IGetBookView{
+import org.w3c.dom.Text;
+
+public class DoubanBookDetailActivity extends AppCompatActivity implements IGetBookView {
 
     private DoubanBooksPresenter presenter;
 
-//    private TextView textView;
+    private ImageView img_Cover;
+
+    private TextView txt_Title;
+
+    private CollapsingToolbarLayout collapsingToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_douban_book_detail);
-//        textView = (TextView)findViewById(R.id.txt_BookDetail);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolBar_doubanBookDetail);
+        img_Cover = (ImageView) findViewById(R.id.img_doubanBookDetail);
+        txt_Title = (TextView) findViewById(R.id.txt_doubanBookDetail);
+        collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbar_doubanBookDetail);
+
+        setSupportActionBar(toolbar);
+
         presenter = new DoubanBooksPresenter(this);
         Intent intent = getIntent();
-        presenter.getBookById(this,intent.getStringExtra("BookId"));
+        presenter.getBookById(this, intent.getStringExtra("BookId"));
     }
 
     @Override
     public void getBookSuccess(Book book) {
-//        StringBuilder sb = new StringBuilder();
-//        sb.append(book.getTitle() + "\n")
-//                .append(book.getAuthor()+"\n");
-//        textView.setText(sb.toString());
+        Glide.with(this).
+                load(book.getImage()).
+                override(300,300).
+                fitCenter().
+                into(img_Cover);
+        txt_Title.setText(book.getTitle());
+        collapsingToolbar.setTitle(book.getTitle());
     }
 
     @Override
