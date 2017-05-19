37f1994a80142b35ca6f9880e9e5992fb28a6ee4
yiguohan <yiguohan@gmail.com>
2017-05-19 10:34:07 +0800

    添加MyBookDetailActivity

---
+++ app/src/main/java/com/yiguohan/easyreading/ViewImpls/MyBookDetailActivity.java
@@ -0,0 +1,15 @@
+package com.yiguohan.easyreading.ViewImpls;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yiguohan.easyreading.R;
+
+public class MyBookDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_book_detail);
+    }
+}
