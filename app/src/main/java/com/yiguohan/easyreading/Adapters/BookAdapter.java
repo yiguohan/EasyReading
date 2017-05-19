1d8618dcdd0bdd1c4e1e17d2a2f75096b91eda69
yiguohan <yiguohan@gmail.com>
2017-05-19 14:20:22 +0800

    设置MyBookDetailActivity基本布局

--- app/src/main/java/com/yiguohan/easyreading/Adapters/BookAdapter.java
+++ app/src/main/java/com/yiguohan/easyreading/Adapters/BookAdapter.java
@@ -1,6 +1,7 @@
 package com.yiguohan.easyreading.Adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -12,6 +13,7 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.yiguohan.easyreading.Beans.DoubanBooks.Book;
 import com.yiguohan.easyreading.R;
+import com.yiguohan.easyreading.ViewImpls.MyBookDetailActivity;
 
 import java.util.List;
 
@@ -50,6 +52,14 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.ViewHolder> {
             mContext = parent.getContext();
         }
         View view = LayoutInflater.from(mContext).inflate(R.layout.my_book_item,parent,false);
+        final ViewHolder holder = new ViewHolder(view);
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, MyBookDetailActivity.class);
+                mContext.startActivity(intent);
+            }
+        });
         return new ViewHolder(view);
     }
 
--- app/src/main/java/com/yiguohan/easyreading/ViewImpls/MyBookDetailActivity.java
+++ app/src/main/java/com/yiguohan/easyreading/ViewImpls/MyBookDetailActivity.java
@@ -1,15 +1,60 @@
 package com.yiguohan.easyreading.ViewImpls;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import com.yiguohan.easyreading.Base.BaseActivity;
 import com.yiguohan.easyreading.R;
 
-public class MyBookDetailActivity extends AppCompatActivity {
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+
+import tech.linjiang.suitlines.SuitLines;
+import tech.linjiang.suitlines.Unit;
+
+public class MyBookDetailActivity extends BaseActivity {
+
+    SuitLines suitLines;
+
+    private int curCount = 1;
+
+    private float textSize = 8;
+
+    private int[] color = {Color.RED, Color.GRAY, 0xFFF76055, 0xFF9B3655, 0xFFF7A055};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_book_detail);
+        suitLines = (SuitLines) findViewById(R.id.suitlines);
+        init(curCount);
+    }
+
+    public void init(int count) {
+        if (count <= 0) {
+            count = 0;
+        }
+        if (count == 1) {
+            List<Unit> lines = new ArrayList<>();
+            for (int i = 0; i < 14; i++) {
+                lines.add(new Unit(new SecureRandom().nextInt(48), i + "d"));
+            }
+            suitLines.feedWithAnim(lines);
+            return;
+        }
+
+        SuitLines.LineBuilder builder = new SuitLines.LineBuilder();
+        for (int j = 0; j < count; j++) {
+            List<Unit> lines = new ArrayList<>();
+            for (int i = 0; i < 50; i++) {
+                lines.add(new Unit(new SecureRandom().nextInt(128), "" + i));
+            }
+            builder.add(lines, new int[]{color[new SecureRandom().nextInt(4)], color[new SecureRandom().nextInt(4)], color[new SecureRandom().nextInt(4)]});
+        }
+        builder.build(suitLines, true);
+
     }
 }
