e45e6bd2da2fd8f5eff8d69a98e4bfd6c76eadca
yiguohan <yiguohan@gmail.com>
2017-05-20 11:49:32 +0800

    登陆和注册页面设置状态栏Transparent

--- app/src/main/java/com/yiguohan/easyreading/ViewImpls/LoginActivity.java
+++ app/src/main/java/com/yiguohan/easyreading/ViewImpls/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.yiguohan.easyreading.ViewImpls;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -41,8 +44,16 @@ public class LoginActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+        if(Build.VERSION.SDK_INT >= 21){
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_FULLSCREEN|View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+
     }
 
+    //// TODO: 2017/5/19 返回的数据没有拿到
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
--- app/src/main/java/com/yiguohan/easyreading/ViewImpls/SignUpActivity.java
+++ app/src/main/java/com/yiguohan/easyreading/ViewImpls/SignUpActivity.java
@@ -1,8 +1,11 @@
 package com.yiguohan.easyreading.ViewImpls;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 import com.yiguohan.easyreading.Base.BaseActivity;
@@ -31,5 +34,11 @@ public class SignUpActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         ButterKnife.bind(this);
+        if(Build.VERSION.SDK_INT >= 21){
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_FULLSCREEN|View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
     }
 }
