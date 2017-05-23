e6d99fd6d5e72236aaf1124dbe16bffb0eb0a9f4
yiguohan <yiguohan@gmail.com>
2017-05-23 19:00:37 +0800

    添加阅读记录的Activity ReadingActivity

---
+++ app/src/main/java/com/yiguohan/easyreading/ViewImpls/ReadingActivity.java
@@ -0,0 +1,16 @@
+package com.yiguohan.easyreading.ViewImpls;
+
+
+import android.os.Bundle;
+
+import com.yiguohan.easyreading.Base.BaseActivity;
+import com.yiguohan.easyreading.R;
+
+public class ReadingActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reading);
+    }
+}
