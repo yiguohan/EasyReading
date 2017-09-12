71e83ce65f3f7366241d953cf064682cd896f535
yiguohan <yiguohan@gmail.com>
2017-09-12 20:37:14 +0800

    EntryActivity

---
+++ app/src/main/java/com/yiguohan/easyreading/ViewImpls/EntryActivity.java
@@ -0,0 +1,16 @@
+package com.yiguohan.easyreading.ViewImpls;
+
+import android.os.Bundle;
+
+import com.yiguohan.easyreading.Base.BaseActivity;
+import com.yiguohan.easyreading.R;
+
+public class EntryActivity extends BaseActivity {
+
+    //TODO 完成欢迎页逻辑 加Bing每日一图
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry);
+    }
+}
